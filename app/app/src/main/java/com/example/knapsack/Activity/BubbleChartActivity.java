e7925a8ddf414a1009a51e6bd1f7f8bf086a4b2c
BYG-0105 <201971010259@nwnu.edu.cn>
2022-04-04 10:29:51 +0800

    绘制图像代码

---
+++ app/app/src/main/java/com/example/knapsack/Activity/BubbleChartActivity.java
@@ -0,0 +1,14 @@
+package com.example.knapsack.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BubbleChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bubble);
+    }
+}
